5905de1b3e551131bc07bb3a896814749ba672c0
Vojtech Hyza <vhyza@vhyza.eu>
2014-02-14 13:52:47 +0100

    Updated sass to version 3.3.0.rc.3

--- lib/sass-rails-source-maps/sass_importer.rb
+++ lib/sass-rails-source-maps/sass_importer.rb
@@ -1,8 +1,9 @@
 module SassRailsSourceMaps
   class SassImporter < Sprockets::SassImporter
 
-    def public_url(uri)
-      "/#{SOURCE_MAPS_DIRECTORY}/#{File.basename(uri)}"
+    def public_url(uri, sourcemap_dir = nil)
+      source_maps_directory = sourcemap_dir ? sourcemap_dir.sub(Rails.root.join('public').to_s, '') : "/#{SOURCE_MAPS_DIRECTORY}"
+      "#{source_maps_directory}/#{File.basename(uri)}"
     end
 
   end
--- sass-rails-source-maps.gemspec
+++ sass-rails-source-maps.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |gem|
 
   gem.add_dependency "rails"
   gem.add_dependency "sprockets", ">= 2.3.0"
-  gem.add_dependency "sass",      ">= 3.3.0.rc.2"
+  gem.add_dependency "sass",      ">= 3.3.0.rc.3"
 
   gem.add_development_dependency "bundler", "~> 1.3"
   gem.add_development_dependency "rake"
